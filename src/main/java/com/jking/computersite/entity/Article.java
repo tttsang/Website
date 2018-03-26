343fcd5fcf426c8b56453b8c8f4112c4cb4e1299
bin <411036847@qq.com>
2018-03-26 18:55:59 +0800

    新增了文章的查找模块

---
+++ src/main/java/com/jking/computersite/entity/Article.java
@@ -0,0 +1,85 @@
+package com.jking.computersite.entity;
+
+import java.util.Date;
+
+public class Article {
+    private String id;
+
+    private Integer isshow;
+
+    private Date time;
+
+    private String title;
+
+    private String author;
+
+    private String auditor;
+
+    private String content;
+
+    private String pictureUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public Integer getIsshow() {
+        return isshow;
+    }
+
+    public void setIsshow(Integer isshow) {
+        this.isshow = isshow;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author == null ? null : author.trim();
+    }
+
+    public String getAuditor() {
+        return auditor;
+    }
+
+    public void setAuditor(String auditor) {
+        this.auditor = auditor == null ? null : auditor.trim();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl == null ? null : pictureUrl.trim();
+    }
+}
---
+++ src/main/java/com/jking/computersite/mapper/ArticleMapper.java
@@ -0,0 +1,17 @@
+package com.jking.computersite.mapper;
+
+import com.jking.computersite.entity.Article;
+
+public interface ArticleMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(Article record);
+
+    int insertSelective(Article record);
+
+    Article selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(Article record);
+
+    int updateByPrimaryKey(Article record);
+}
