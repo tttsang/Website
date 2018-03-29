ecb28ef269e6fffbcf510d1e4804960fdec49be4
bin <411036847@qq.com>
2018-03-29 12:34:19 +0800

    新增了文章的查找模块

--- src/main/java/com/jking/computersite/VO/ResultVO.java
+++ src/main/java/com/jking/computersite/VO/ResultVO.java
@@ -15,4 +15,28 @@ public class ResultVO<T> {
 
     //具体信息
     private T data;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
